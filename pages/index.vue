8add34c6aa0aa15fcb20ddbcc05dacefe0342a5b
wildanhkm <wildanhakim98@gmail.com>
2022-11-22 19:42:59 +0700

    add bryntum kanban

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial />
+  <div>
+    <BryntumProjectModel ref="project" v-bind="projectConfig" :tasks="tasks" />
+    <BryntumTaskBoard ref="taskboard" v-bind="taskBoardConfig" />
+  </div>
 </template>
 
 <script>
+import { BryntumProjectModel, BryntumTaskBoard } from '@bryntum/taskboard-vue-3'
 export default {
   name: 'IndexPage',
+
+  components: {
+    BryntumProjectModel,
+    BryntumTaskBoard,
+  },
+
+  data() {
+    return {
+      taskBoardConfig: {
+        columns: ['todo', 'doing', 'done'],
+        columnField: 'status',
+      },
+      projectConfig: {},
+      tasks: [
+        { id: 1, name: 'My first task', status: 'doing' },
+        { id: 2, name: 'My second task', status: 'todo' },
+      ],
+    }
+  },
 }
 </script>
